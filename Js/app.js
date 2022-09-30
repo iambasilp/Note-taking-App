7e22313a7fdbce2f804682a40826fa77f291a1a1
iambasilp <basilxg321@gmail.com>
2022-09-30 18:44:49 +0530

    some changes applied

--- Js/app.js
+++ Js/app.js
@@ -1,5 +1,6 @@
 const addbox = document.querySelector(".add-box");
 const popupBox = document.querySelector(".popup-box");
+const popuptitle = document.querySelector('header p')
 const closeIcon = popupBox.querySelector("header i");
 const titleTag = popupBox.querySelector("input");
 const descTag = popupBox.querySelector("textarea");
@@ -25,11 +26,15 @@ addbox.addEventListener("click", () => {
 
 closeIcon.addEventListener("click", () => {
   popupBox.classList.remove("show");
+  addbutton.innerHTML = 'Add note'
+  popuptitle.innerHTML = 'Add a new note'
+  titleTag.value = '';
+  descTag.value = '';
 });
 
 function shownotes() {
     document.querySelectorAll('.note').forEach((note)=>{note.remove()})
-  notes.forEach((note) => {
+  notes.forEach((note,id) => {
       let liTag = `      <li class="note">
       <div class="details">
         <p>${note.title}</p>
@@ -40,10 +45,10 @@ function shownotes() {
       <div class="bottom-content">
         <span>April 3, 2022</span>
         <div class="settings">
-          <i class="ri-more-fill"></i>
+          <i onclick = 'showmenu(this)' class="ri-more-fill"></i>
           <ul class="menu">
-            <li><i class="ri-pencil-line"></i>Edit</li>
-            <li><i class="ri-delete-bin-7-line"></i>Delete</li>
+            <li onclick = "editnote(${id} , '${note.title}' , '${note.description}')"><i class="ri-pencil-line"></i>Edit</li>
+            <li onclick= 'deletenote(${id})'><i class="ri-delete-bin-7-line"></i>Delete</li>
 
           </ul>
         </div>
@@ -53,6 +58,34 @@ function shownotes() {
   });
 }
 shownotes();
+
+function showmenu(selectednote){
+  let notemenu = selectednote.parentElement.lastElementChild
+  notemenu.classList.add('active')
+  document.addEventListener('click',(e)=>{
+    if(e.target.tagName != "I" || e.target != selectednote){
+      notemenu.classList.remove('active')
+    }
+  })
+}
+function deletenote(deleteId){
+  notes.splice(deleteId,1)
+  shownotes()
+  localStorage.setItem('notes-list', JSON.stringify(notes))
+}
+addbox.addEventListener('click',()=>{
+  titleTag.focus();
+  popupBox.classList.add('show')
+})
+function editnote(noteid,title,desc){
+  addbox.click()
+  titleTag.value = title;
+  descTag.value = desc;
+  addbutton.innerHTML = 'Update notes'
+  popuptitle.innerHTML = 'Update notes'
+  console.log(noteid,title,desc);
+}
+
 
 addbutton.addEventListener("click", (e) => {
   e.preventDefault();
@@ -70,6 +103,7 @@ addbutton.addEventListener("click", (e) => {
       description: notedesc,
       date: `${month} ${daynumber}, ${year}`,
     };
+
     notes.push(noteInfo);
     localStorage.setItem("notes-list", JSON.stringify(notes));
     closeIcon.click();
